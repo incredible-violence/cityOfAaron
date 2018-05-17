b455517aaa2961bda6bdb2a88dbeb35b93a5af2d
waabbajak <waabbajak@gmail.com>
2018-05-16 20:28:45 -0700

    Initial Commit

---
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -0,0 +1,22 @@
+/*
+ * Main class file for cityOfAaron
+ * CIT_260
+ * Spring 2018
+ * Team: James Kennedy,
+ */
+package cityofaaron;
+
+/**
+ *
+ * @author waabb
+ */
+public class CityOfAaron {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
