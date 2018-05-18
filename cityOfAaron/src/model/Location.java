6902907beb51cddef38af5dd3f5831da09afd4b5
Mallory <dib17003@byui.edu>
2018-05-17 20:41:28 -1000

    Class instance variables and getters and setters added.

--- cityOfAaron/src/model/Location.java
+++ cityOfAaron/src/model/Location.java
@@ -1,14 +1,80 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Location class file for cityOfAaron
+ * CIT_260
+ * Spring 2018
+ * Team: James Kennedy, Mallory Di Bartolo
  */
 package model;
+
+import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
  * @author mallorydibartolo
  */
-public class Location {
+public class Location implements Serializable {
+    
+    //class instance variables
+    private String description; 
+    private String symbol; 
+
+    public Location() {
+    }
+
+    
+    
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 43 * hash + Objects.hashCode(this.description);
+        hash = 43 * hash + Objects.hashCode(this.symbol);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "description=" + description + ", symbol=" + symbol + '}';
+    }
+    
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.symbol, other.symbol)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
