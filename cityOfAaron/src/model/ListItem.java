2fe50f6a6e4e0885e9bfd2e438d7a6d26ed363c2
Mallory <dib17003@byui.edu>
2018-05-18 16:45:37 -1000

    ListItem class assignment completed 05/18

--- cityOfAaron/src/model/ListItem.java
+++ cityOfAaron/src/model/ListItem.java
@@ -16,4 +16,69 @@ import java.util.Objects;
  */
 public class ListItem implements Serializable {
     
+    //class instance variables
+    private String name; 
+    private int number;
+
+    public ListItem() {
+    }
+    
+    //getter: name
+    public String getName() {
+        return name;
+    }
+    
+    //setter: name
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    //getter: number
+    public int getNumber() {
+        return number;
+    }
+
+    //setter: number
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + Objects.hashCode(this.name);
+        hash = 23 * hash + this.number;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "ListItem{" + "name=" + name + ", number=" + number + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ListItem other = (ListItem) obj;
+        if (this.number != other.number) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
 }
