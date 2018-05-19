8c7dc1c8307ccbbcca3f9182819c12a982ddc8ec
Mallory <dib17003@byui.edu>
2018-05-18 16:19:56 -1000

    Commit ListItem Class

---
+++ cityOfAaron/src/model/ListItem.java
@@ -0,0 +1,19 @@
+/*
+ * Location class file for cityOfAaron
+ * CIT_260
+ * Spring 2018
+ * Team: James Kennedy, Mallory Di Bartolo
+ */
+
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author mallorydibartolo
+ */
+public class ListItem implements Serializable {
+    
+}
