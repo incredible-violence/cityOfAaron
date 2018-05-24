da2843766a7eecdaa0ac3c3d57eaca1558f00aeb
waabbajak <waabbajak@gmail.com>
2018-05-23 19:52:50 -0700

    Updated Game Class
    updated Game Class with required variables

--- cityOfAaron/src/model/Game.java
+++ cityOfAaron/src/model/Game.java
@@ -23,7 +23,13 @@ public class Game implements Serializable {
      **********************/
     
     // private variables
-    private Player thePlayer; 
+    private Map theMap;
+    private Player thePlayer;
+    private CropData cropdata;
+    private ArrayList<ListItem> animals = new ArrayList<ListItem>();
+    private ArrayList<ListItem> tools = new ArrayList<ListItem>();
+    private ArrayList<ListItem> provisions = new ArrayList<ListItem>();
+    private Boolean done;
     
     //Attempt at implementing the ListItem-Game relationship -Mallory 
     //private ArrayList<ListItem> name = new ArrayList<ListItem>(); 
@@ -42,20 +48,73 @@ public class Game implements Serializable {
         this.thePlayer = thePlayer;
     }
     
+    // variable constructor
+    public Game(Map theMap, Player thePlayer, CropData cropdata, Boolean done) {
+        this.theMap = theMap;
+        this.thePlayer = thePlayer;
+        this.cropdata = cropdata;
+        this.done = done;
+    }
+
     /***********************
      * Getters and Setters
      **********************/
-    
-    // getter for thePlayer
     public Player getThePlayer() {
         return thePlayer;
     }
     
-    // setter for thePlayer
     public void setThePlayer(Player thePlayer) {
         this.thePlayer = thePlayer;
     }
-    
+
+    public Map getTheMap() {
+        return theMap;
+    }
+
+    public void setTheMap(Map theMap) {
+        this.theMap = theMap;
+    }
+
+    public CropData getCropdata() {
+        return cropdata;
+    }
+
+    public void setCropdata(CropData cropdata) {
+        this.cropdata = cropdata;
+    }
+
+    public ArrayList<ListItem> getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(ArrayList<ListItem> animals) {
+        this.animals = animals;
+    }
+
+    public ArrayList<ListItem> getTools() {
+        return tools;
+    }
+
+    public void setTools(ArrayList<ListItem> tools) {
+        this.tools = tools;
+    }
+
+    public ArrayList<ListItem> getProvisions() {
+        return provisions;
+    }
+
+    public void setProvisions(ArrayList<ListItem> provisions) {
+        this.provisions = provisions;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+        
     // hashCode()
     @Override
     public int hashCode() {
