1bcb859588a50d752697712d51a337818b823900
waabbajak <waabbajak@gmail.com>
2018-05-31 18:41:19 -0700

    updated header for CropControlTest

--- cityOfAaron/test/control/CropControlTest.java
+++ cityOfAaron/test/control/CropControlTest.java
@@ -1,8 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*********************************************
+ * Class: CropControl
+ * CIT_260
+ * Spring 2018
+ * Team: James Kennedy, Mallory DiBartolo
+ ********************************************/
 package control;
 
 import model.CropData;
